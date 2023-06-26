6da4805da7a2e282d74d0300c416daabbe2b40a8
gain00 <wjdwo1104@gmail.com>
2023-06-26 17:40:19 +0900

    DTO/VO클래스에 lombok 적용

--- src/main/java/wjdwo1104/hello/spring4/model/Member.java
+++ src/main/java/wjdwo1104/hello/spring4/model/Member.java
@@ -1,7 +1,15 @@
 package wjdwo1104.hello.spring4.model;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.Date;
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
 public class Member {
     //
     private String mno;
@@ -11,51 +19,4 @@ public class Member {
     private String regdate;
 
 
-    public Member(String mno, String userid, String name, String email, String regdate) {
-        this.mno = mno;
-        this.userid = userid;
-        this.name = name;
-        this.email = email;
-        this.regdate = regdate;
-    }
-
-    public String getMno() {
-        return mno;
-    }
-
-    public void setMno(String mno) {
-        this.mno = mno;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(String regdate) {
-        this.regdate = regdate;
-    }
 }
