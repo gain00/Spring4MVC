a50b6819e460370d6c149de955542475ea4670d7
gain00 <wjdwo1104@gmail.com>
2023-06-28 18:03:27 +0900

    Member 기본 생성자 추가

--- src/main/java/wjdwo1104/hello/spring4/model/Member.java
+++ src/main/java/wjdwo1104/hello/spring4/model/Member.java
@@ -1,19 +1,19 @@
 package wjdwo1104.hello.spring4.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Date;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+//getter/setter 생성자등을 자동으로 생성
 public class Member {
     //
     private String mno;
     private String userid;
+    private String passwd;
     private String name;
     private String email;
     private String regdate;
