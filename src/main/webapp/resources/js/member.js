2848093e42ff460a0669bdbb137230e1af8c7af4
gain00 <wjdwo1104@gmail.com>
2023-06-28 14:57:18 +0900

    Member 관련 자바스크립트 작성

---
+++ src/main/webapp/resources/js/member.js
@@ -0,0 +1,30 @@
+/*join*/
+let joinbtn = document.querySelector("#joinbtn");
+joinbtn?.addEventListener('click',() => {
+
+    let frm = document.forms.joinfrm;
+
+    if (frm.userid.value === '') alert('아이디를작성하세요');
+    else if (frm.passwd.value === '') alert('비밀번호를작성하세요');
+    else if (frm.repwd.value !== frm.passwd.value) alert('비밀번호가 일치하지않아요');
+    else if (frm.name.value === '') alert('이름를작성하세요');
+    else if (frm.email.value === '') alert('이메일를작성하세요');
+    else {
+        frm.method = 'post';
+        frm.action = '/member/join';
+        frm.submit();
+    }
+});
+
+/*login*/
+let loginbtn = document.querySelector("#loginbtn");
+loginbtn?.addEventListener('click',() =>{
+    let frm = document.forms.loginfrm;
+    if(frm.userid.value === '') alert();
+    else if(frm.passwd.value === '') alert();
+    else {
+        frm.method = 'post';
+        frm.action = '/member/login';
+        frm.submit();
+    }
+});
