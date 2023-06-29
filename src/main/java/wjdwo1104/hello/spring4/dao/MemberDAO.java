09a714f4ee1fa8bb9901c614e65bab4ffe1c66c6
gain00 <wjdwo1104@gmail.com>
2023-06-29 15:29:20 +0900

    MemberDao loginMember 구현

--- src/main/java/wjdwo1104/hello/spring4/dao/MemberDAO.java
+++ src/main/java/wjdwo1104/hello/spring4/dao/MemberDAO.java
@@ -4,4 +4,5 @@ import wjdwo1104.hello.spring4.model.Member;
 
 public interface MemberDAO {
     int insertMember(Member m);
+    Member loginMember(Member m);
 }
--- src/main/java/wjdwo1104/hello/spring4/dao/MemberDAOImpl.java
+++ src/main/java/wjdwo1104/hello/spring4/dao/MemberDAOImpl.java
@@ -3,13 +3,18 @@ package wjdwo1104.hello.spring4.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import wjdwo1104.hello.spring4.model.Member;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
     // sql.properties에 작성한 sql 불러오기
     @Value("#{sql['insertMember']}") private String insertSQL;
+    @Value("#{sql['loginMember']}") private String loginSQL;
 
 
     @Autowired
@@ -24,5 +29,29 @@ public class MemberDAOImpl implements MemberDAO{
         };
         //쿼리실행 : update(sql문, 매개변수)
         return jdbcTemplate.update(insertSQL, params);
+    }
+
+
+    public Member loginMember(Member m) {
+        //매개변수 정의
+        Object[] params = new Object[] {
+                m.getUserid(),m.getPasswd(),
+
+        };
+        //로그인 매퍼 선언
+        RowMapper<Member> mapper= new LoginMapper();
+
+        //쿼리실행 : update(sql문, 매개변수) - 단일갑 봔환
+        return jdbcTemplate.queryForObject(loginSQL, params, mapper);
+    }
+
+    private class LoginMapper implements RowMapper<Member> {
+        @Override
+        public Member mapRow(ResultSet rs, int i) throws SQLException {
+            Member m = new Member();
+            m.setUserid(rs.getString(1));
+            m.setName(rs.getString(2));
+            return m;
+        }
     }
 }
