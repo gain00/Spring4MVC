45c3373de288b0490cc4854edf217dccbfd8b424
gain00 <wjdwo1104@gmail.com>
2023-06-29 15:29:53 +0900

    MemberService loginMember 단위테스트

--- src/test/java/service/MemberServiceUnitTest.java
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -12,6 +12,7 @@ import wjdwo1104.hello.spring4.model.Member;
 import wjdwo1104.hello.spring4.service.MemberService;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/servlet-context.xml","classpath:spring/root-context.xml"})
@@ -27,5 +28,16 @@ public class MemberServiceUnitTest {
         Member m = new Member(null,"abc123a","987xyz","abc123","abc123@987xyz",null);
         //assertEquals(테스트메서드, 검증값)
         assertEquals(msrv.saveMember(m),true);
+    }
+
+    @Test
+    //Transactional 데이터에 실제값은 안들어감
+    public void loginMember() throws Exception {
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+        //assertEquals(테스트메서드, 검증값)
+        assertEquals(msrv.loginMember(m), true);
+
     }
 }
