9dc4813a5f3fa0ad60e2c2c6b03b033ffbf382ac
gain00 <wjdwo1104@gmail.com>
2023-06-30 16:53:42 +0900

    boardDAO : readBoard 구현

---
+++ src/main/java/wjdwo1104/hello/spring4/service/BoardService.java
@@ -0,0 +1,11 @@
+package wjdwo1104.hello.spring4.service;
+
+import wjdwo1104.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<Board> readBoard(int cpg);
+
+}
---
+++ src/main/java/wjdwo1104/hello/spring4/service/BoardServiceImpl.java
@@ -0,0 +1,20 @@
+package wjdwo1104.hello.spring4.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wjdwo1104.hello.spring4.dao.BoardDAO;
+import wjdwo1104.hello.spring4.model.Board;
+
+import java.util.List;
+@Service("bsrv")
+public class BoardServiceImpl implements BoardService{
+    @Autowired
+    BoardDAO bdao;
+    @Override
+    public List<Board> readBoard(int cpg) {
+        int snum = (cpg-1) * 15;
+
+
+        return bdao.selectBoard(snum);
+    }
+}
