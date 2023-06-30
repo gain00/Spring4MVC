ec02242d3f7890a8a74c69ac7252ff5d13ee62b9
gain00 <wjdwo1104@gmail.com>
2023-06-30 16:54:00 +0900

    boardDAO : readBoard 단위테스트

---
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import wjdwo1104.hello.spring4.dao.MemberDAO;
+import wjdwo1104.hello.spring4.model.Board;
+import wjdwo1104.hello.spring4.model.Member;
+import wjdwo1104.hello.spring4.service.BoardService;
+import wjdwo1104.hello.spring4.service.MemberService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml","classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class BoardServiceUnitTest {
+    //테스트할 대상 객체를 주입받음
+    @Autowired
+    BoardService bsrv;
+
+
+    @Test
+    //Transactional 데이터에 실제값은 안들어감
+    public void readBoard() throws Exception {
+        int cpage = 1;
+        List<Board> results = bsrv.readBoard(cpage);
+
+        assertEquals(results.size(),15);
+        System.out.println(results);
+    }
+
+
+}
