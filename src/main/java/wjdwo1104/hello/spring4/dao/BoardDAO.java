f6dae418ee8af7f8601621186edc7ab1875ef1ba
gain00 <wjdwo1104@gmail.com>
2023-06-30 16:52:28 +0900

    boardDAO : selectBoard 구현

---
+++ src/main/java/wjdwo1104/hello/spring4/dao/BoardDAO.java
@@ -0,0 +1,9 @@
+package wjdwo1104.hello.spring4.dao;
+
+import wjdwo1104.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardDAO {
+    List<Board> selectBoard(int snum);
+}
